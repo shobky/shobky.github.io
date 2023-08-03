0d23f0d89e6183e4c42ec3133e3c1999585de50f
shobky <shobkyy@gmail.com>
2023-08-03 23:00:11 +0300

    config-next

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ isDev = process.env.NEXT_PUBLIC_ENV === "dev" ? true : false;
 
 const nextConfig = {
   output: "static",
-  basePath: "/",
+  basePath: "",
   images: {
     unoptimized: false,
     domains: ["opengraph.githubassets.com"],
