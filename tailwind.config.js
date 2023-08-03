1ca232a2054cb400621e2dcdc18950fb73f557ae
shobky <shobkyy@gmail.com>
2023-08-03 22:35:28 +0300

    fix-postcss-parse??

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "/app/[title]/page.tsx",
   ],
   theme: {
     extend: {},
