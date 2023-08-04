320b0534624e52b112afe9a4357eb7f9ca665469
shobky <shobkyy@gmail.com>
2023-08-04 11:12:09 +0300

    next-config-was-inside-app-dir?i-hate-my-life

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+
+isDev = process.env.NEXT_PUBLIC_ENV === "dev" ? true : false;
+
+const nextConfig = {
+  assetPrefix: isDev ? "" : "/shobky.github.io",
+  basePath: isDev ? "" : "/shobky.github.io",
+  images: {
+    unoptimized: !isDev && false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "opengraph.githubassets.com",
+        port: "",
+        pathname: "/*/shobky/**",
+      },
+    ]
+  },
+};
+
+
+module.exports = nextConfig;
